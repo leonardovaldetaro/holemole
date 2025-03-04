178654769ab4a4b9cf1b2bf04669cc2c23f8ac81
Leonardo Valdetaro <leonardovaldetaro@gmail.com>
2025-03-04 11:44:18 +0000

    ajustes finos no slide do fim da pagina

--- app/home/_components/slide/index.tsx
+++ app/home/_components/slide/index.tsx
@@ -10,11 +10,12 @@ import 'swiper/scss/pagination';
 
 import Styles from './Slide.module.scss';
 import Button from '../../../_components/button/button';
+import classNames from 'classnames';
 
 import WhiteFishLogo from '../../../_components/white-fish-logo';
+import BlueFish from '../../../_components/blueFish';
 
 import { Nunito } from 'next/font/google';
-import BlueFish from '../../../_components/blueFish';
 
 const nunito = Nunito({
     weight: '500',
@@ -37,11 +38,11 @@ export default function Slide() {
                         <div className={Styles.container__content}>
                             <BlueFish />
                             <h2 className={Styles.container__content__title}>Taco<br /> Tuesday</h2>
-                            <p className={nunito.className}>Taco Tuesday at Holé Molé! Gather your friends and enjoy mouthwatering tacos paired with the best drinks in town. The perfect way to spice up your week! See you after work! Offers and participation may vary by location.</p>
+                            <p className={nunito.className}>Taco Tuesday at Holé Molé! Gather your friends and enjoy mouthwatering tacos paired with the best drinks in town.</p>
                             <Button><WhiteFishLogo />Check out our menu!</Button>
                         </div>
-                        <div className={Styles.slideImage}>
-                            <img src="assets/taco-tuesday.jpg" alt="Taco tuesday" />
+                        <div className={classNames(Styles.slideImage, Styles.slideImage__01)}>
+                            {/* <img src="assets/taco-tuesday.jpg" alt="Taco tuesday" /> */}
                         </div>
                     </div>
                 </SwiperSlide>
@@ -51,11 +52,11 @@ export default function Slide() {
                         <div className={Styles.container__content}>
                             <BlueFish />
                             <h2 className={Styles.container__content__title}>happy<br />hour</h2>
-                            <p className={nunito.className}>Nothing better than great drinks and amazing flavors to end the day. Unwind with us, enjoy refreshing cocktails, and savor delicious bites. The perfect way to relax and make the most of your evening!.</p>
+                            <p className={nunito.className}>Nothing better than great drinks and amazing flavors to end the day. Unwind with us, enjoy refreshing cocktails, and savor delicious bites.</p>
                             <Button><WhiteFishLogo />Check out our menu!</Button>
                         </div>
-                        <div className={Styles.slideImage}>
-                            <img src="assets/hapy-hour.jpg" alt="Taco tuesday" />
+                        <div className={classNames(Styles.slideImage, Styles.slideImage__02)}>
+                            {/* <img src="assets/hapy-hour.jpg" alt="Taco tuesday" /> */}
                         </div>
                     </div>
                 </SwiperSlide>
@@ -65,11 +66,11 @@ export default function Slide() {
                         <div className={Styles.container__content}>
                             <BlueFish />
                             <h2 className={Styles.container__content__title}>sunday<br /> funday</h2>
-                            <p className={nunito.className}>Nothing better than great drinks, amazing flavors, and delicious desserts to make the most of your Sunday. Unwind with us, enjoy refreshing cocktails, savor tasty bites, and treat yourself to something sweet. The perfect way to wrap up the weekend!</p>
+                            <p className={nunito.className}>Nothing better than great drinks, amazing flavors, and delicious desserts to make the most of your Sunday.</p>
                             <Button><WhiteFishLogo />Check out our menu!</Button>
                         </div>
-                        <div className={Styles.slideImage}>
-                            <img src="assets/sunday-funday.jpg" alt="Taco tuesday" />
+                        <div className={classNames(Styles.slideImage, Styles.slideImage__03)}>
+                            {/* <img src="assets/sunday-funday.jpg" alt="Taco tuesday" /> */}
                         </div>
                     </div>
                 </SwiperSlide>
